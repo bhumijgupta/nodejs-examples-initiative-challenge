41615db3334618ac79ef979ab915e9af6a2f5311
bhumijgupta <bhumijgupta@gmail.com>
2020-09-13 22:41:47 +0530

    test:  Add test for /dependencies
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

--- test/index.js
+++ test/index.js
@@ -1,33 +1,43 @@
-const tape = require('tape')
-const bent = require('bent')
-const getPort = require('get-port')
+const tape = require("tape");
+const bent = require("bent");
+const getPort = require("get-port");
+const fs = require("fs");
+const path = require("path");
 
-const server = require('../')
+const server = require("../server");
 
-const getJSON = bent('json')
-const getBuffer = bent('buffer')
+const getJSON = bent("json");
+const getBuffer = bent("buffer");
 
 // Use `nock` to prevent live calls to remote services
 
-const context = {}
+const context = {};
 
-tape('setup', async function (t) {
-	const port = await getPort()
-	context.server = server.listen(port)
-	context.origin = `http://localhost:${port}`
+tape("setup", async function (t) {
+  const port = await getPort();
+  context.server = server.listen(port);
+  context.origin = `http://localhost:${port}`;
 
-	t.end()
-})
+  t.end();
+});
 
-tape('should get dependencies', async function (t) {
-	const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
-
-	// assertions etc
-})
+tape("should get dependencies", async function (t) {
+  // Read package.json to get actual dependencies
+  const package = fs.readFileSync(path.join(__dirname, "../package.json"));
+  const { dependencies: actualDependencies } = JSON.parse(package);
+  //   Get actual output
+  const html = (await getBuffer(`${context.origin}/dependencies`)).toString();
+  //   Check if each dependency in package.json is included
+  Object.keys(actualDependencies).forEach((dependency) => {
+    const version = actualDependencies[dependency];
+    const dependencyIncluded = html.includes(`${dependency} - ${version}`);
+    t.true(dependencyIncluded, `${dependency} included`);
+  });
+});
 
 // more tests
 
-tape('teardown', function (t) {
-	context.server.close()
-	t.end()
-})
+tape("teardown", function (t) {
+  context.server.close();
+  t.end();
+});
