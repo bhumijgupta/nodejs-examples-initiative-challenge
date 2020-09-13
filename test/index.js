f795872cf2a3d86151c80ca059242ecc9270254a
bhumijgupta <bhumijgupta@gmail.com>
2020-09-13 23:28:54 +0530

    test: Add test for /latest-releases
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

--- test/index.js
+++ test/index.js
@@ -12,7 +12,10 @@ const getBuffer = bent("buffer");
 const sampleVersion = require("./sampleVersions.json");
 
 // Use `nock` to prevent live calls to remote services
-nock("https://nodejs.org").get("/dist/index.json").reply(200, sampleVersion);
+nock("https://nodejs.org")
+  .persist()
+  .get("/dist/index.json")
+  .reply(200, sampleVersion);
 const context = {};
 
 tape("setup", async function (t) {
@@ -146,6 +149,120 @@ tape("should get minimum secure versions", async function (t) {
       modules: "72",
       lts: "Erbium",
       security: true,
+    },
+    "v12 should match"
+  );
+});
+
+tape("should get latest version", async function (t) {
+  const response = await getJSON(`${context.origin}/latest-releases`);
+  t.deepEqual(
+    response["v14"],
+    {
+      version: "v14.10.1",
+      date: "2020-09-10",
+      files: [
+        "aix-ppc64",
+        "headers",
+        "linux-arm64",
+        "linux-armv7l",
+        "linux-ppc64le",
+        "linux-s390x",
+        "linux-x64",
+        "osx-x64-pkg",
+        "osx-x64-tar",
+        "src",
+        "win-x64-7z",
+        "win-x64-exe",
+        "win-x64-msi",
+        "win-x64-zip",
+        "win-x86-7z",
+        "win-x86-exe",
+        "win-x86-msi",
+        "win-x86-zip",
+      ],
+      npm: "6.14.8",
+      v8: "8.4.371.19",
+      uv: "1.39.0",
+      zlib: "1.2.11",
+      openssl: "1.1.1g",
+      modules: "83",
+      lts: false,
+      security: false,
+    },
+    "v14 should match"
+  );
+  t.deepEqual(
+    response["v13"],
+    {
+      version: "v13.14.0",
+      date: "2020-04-29",
+      files: [
+        "aix-ppc64",
+        "headers",
+        "linux-arm64",
+        "linux-armv7l",
+        "linux-ppc64le",
+        "linux-s390x",
+        "linux-x64",
+        "osx-x64-pkg",
+        "osx-x64-tar",
+        "src",
+        "sunos-x64",
+        "win-x64-7z",
+        "win-x64-exe",
+        "win-x64-msi",
+        "win-x64-zip",
+        "win-x86-7z",
+        "win-x86-exe",
+        "win-x86-msi",
+        "win-x86-zip",
+      ],
+      npm: "6.14.4",
+      v8: "7.9.317.25",
+      uv: "1.37.0",
+      zlib: "1.2.11",
+      openssl: "1.1.1g",
+      modules: "79",
+      lts: false,
+      security: false,
+    },
+    "v13 should match"
+  );
+  t.deepEqual(
+    response["v12"],
+    {
+      version: "v12.18.3",
+      date: "2020-07-22",
+      files: [
+        "aix-ppc64",
+        "headers",
+        "linux-arm64",
+        "linux-armv7l",
+        "linux-ppc64le",
+        "linux-s390x",
+        "linux-x64",
+        "osx-x64-pkg",
+        "osx-x64-tar",
+        "src",
+        "sunos-x64",
+        "win-x64-7z",
+        "win-x64-exe",
+        "win-x64-msi",
+        "win-x64-zip",
+        "win-x86-7z",
+        "win-x86-exe",
+        "win-x86-msi",
+        "win-x86-zip",
+      ],
+      npm: "6.14.6",
+      v8: "7.8.279.23",
+      uv: "1.38.0",
+      zlib: "1.2.11",
+      openssl: "1.1.1g",
+      modules: "72",
+      lts: "Erbium",
+      security: false,
     },
     "v12 should match"
   );
